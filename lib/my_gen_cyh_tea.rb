322ea8d6813795423e46c7683c3b6cd40385cb50
whymesay <cyhyx521@gmail.com>
2024-03-03 10:17:23 +0800

    Initial commit

---
+++ lib/my_gen_cyh_tea.rb
@@ -0,0 +1,6 @@
+require "my_gen_cyh_tea/version"
+
+module MyGenCyhTea
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/my_gen_cyh_tea/version.rb
@@ -0,0 +1,3 @@
+module MyGenCyhTea
+  VERSION = "0.1.0"
+end
